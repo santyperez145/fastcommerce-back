8b0f8d440611dbaf909d6843f6b951dee7f7ec22
santyperez145 <133786855+santyperez145@users.noreply.github.com>
2023-08-12 19:40:12 -0300

    advances card, user orders (#8)

--- controllers/buys/purchaseControllers.js
+++ controllers/buys/purchaseControllers.js
@@ -1,89 +1,132 @@
-// controllers/purchaseController.js
-import mercadopago from 'mercadopago';
-import Purchase from '../../models/Purchase.js';
+  // controllers/purchaseController.js
+  import mercadopago from 'mercadopago';
+  import Purchase from '../../models/Purchase.js';
 
-mercadopago.configure({
-  access_token: process.env.MERCADOPAGO_ACCESS_TOKEN,
-});
+  mercadopago.configure({
+    access_token: process.env.MERCADOPAGO_ACCESS_TOKEN,
+  });
 
-const generateOrderNumber = () => {
-  const timestamp = new Date().getTime(); // Obtén el timestamp actual
-  const randomDigits = Math.floor(Math.random() * 10000); // Genera 4 dígitos aleatorios
-  return `ORD-${timestamp}-${randomDigits}`;
-};
+  const generateOrderNumber = () => {
+    const timestamp = new Date().getTime(); // Obtén el timestamp actual
+    const randomDigits = Math.floor(Math.random() * 10000); // Genera 4 dígitos aleatorios
+    return `ORD-${timestamp}-${randomDigits}`;
+  };
 
-const createPurchase = async (req, res) => {
-  const { user_id, cartItems } = req.body; // Datos necesarios para la compra
+  const createPurchase = async (req, res) => {
+    const { user_id, cartItems } = req.body; // Datos necesarios para la compra
 
-  try {
-    const preferenceItems = cartItems.map(item => ({
-      title: item.product.name,
-      unit_price: item.product.price * 607,
-      quantity: item.quantity,
-    }));
-
-
-    const preference = {
-      items: preferenceItems,
-      back_urls: {
-        success: "http://localhost:5173/checkout/success",
-        failure: "http://localhost:5173/checkout/failure",
-        pending: "http://localhost:5173/checkout/pending",
-      },
-      auto_return: "approved",
-      // ... otros campos de la preferencia ...
-    };
-
-    const response = await mercadopago.preferences.create(preference);
-
-    const newPurchase = new Purchase({
-      user_id,
-      items: cartItems.map(item => ({
-        product: item.product,
+    try {
+      const preferenceItems = cartItems.map(item => ({
+        title: item.product.name,
+        unit_price: item.product.price * 607,
         quantity: item.quantity,
-      })),
-      payment_status: 'pending',
-      payment_id: response.body.id,
-      shipping_status: 'pending',
-      order_number: generateOrderNumber(),
-    });
+      }));
 
-    await newPurchase.save();
 
-    res.json({ payment_url: response.body.init_point });
-  } catch (error) {
-    console.error('Error al crear la compra en MercadoPago:', error);
-    res.status(500).json({ error: 'Error al procesar la compra' });
-  }
-};
+      const preference = {
+        items: preferenceItems,
+        back_urls: {
+          success: "http://localhost:5173/dashboard/user/:user_id",
+          failure: "http://localhost:5173/dashboard/user/:user_id",
+          pending: "http://localhost:5173/dashboard/user/:user_id",
+        },
+        auto_return: "approved",
+        // ... otros campos de la preferencia ...
+      };
 
-// controllers/purchaseController.js
-const getOrderDetails = async (req, res) => {
-  const { orderNumber } = req.params;
+      const response = await mercadopago.preferences.create(preference);
 
-  try {
-    const order = await Purchase.findOne({ order_number: orderNumber });
+      const newPurchase = new Purchase({
+        user_id,
+        items: cartItems.map(item => ({
+          product: item.product,
+          quantity: item.quantity,
+        })),
+        payment_status: 'pending',
+        payment_id: response.body.id,
+        shipping_status: 'pending',
+        order_number: generateOrderNumber(),
+      });
 
-    if (!order) {
-      return res.status(404).json({ message: 'Orden no encontrada' });
+      await newPurchase.save();
+
+      res.json({ payment_url: response.body.init_point });
+    } catch (error) {
+      console.error('Error al crear la compra en MercadoPago:', error);
+      res.status(500).json({ error: 'Error al procesar la compra' });
     }
+  };
 
-    // Consultar el estado del pago en Mercado Pago
-    if (order.payment_id) {
-      try {
-        const paymentInfo = await mercadopago.payment.get(order.payment_id);
-        order.payment_status = paymentInfo.body.status;
-        await order.save();
-      } catch (error) {
-        console.error('Error al consultar el estado del pago en Mercado Pago:', error);
+  // controllers/purchaseController.js
+  const getOrderDetails = async (req, res) => {
+    const { orderNumber } = req.params;
+
+    try {
+      const order = await Purchase.findOne({ order_number: orderNumber });
+
+      if (!order) {
+        return res.status(404).json({ message: 'Orden no encontrada' });
       }
+
+      // Consultar el estado del pago en Mercado Pago
+      if (order.payment_id) {
+        try {
+          const paymentInfo = await mercadopago.payment.get(order.payment_id);
+          order.payment_status = paymentInfo.body.status;
+          await order.save();
+        } catch (error) {
+          console.error('Error al consultar el estado del pago en Mercado Pago:', error);
+        }
+      }
+
+      res.json(order);
+    } catch (error) {
+      console.error('Error al obtener detalles de la orden:', error);
+      res.status(500).json({ error: 'Error al obtener detalles de la orden' });
     }
+  };
 
-    res.json(order);
-  } catch (error) {
-    console.error('Error al obtener detalles de la orden:', error);
-    res.status(500).json({ error: 'Error al obtener detalles de la orden' });
-  }
-};
+  // controllers/purchaseController.js
+  const getUserOrders = async (req, res) => {
+    const { user_id } = req.params;
+  
+    try {
+      const orders = await Purchase.find({ user_id });
+      res.json(orders);
+    } catch (error) {
+      console.error('Error fetching user orders:', error);
+      res.status(500).json({ error: 'Error fetching user orders' });
+    }
+  };
+  
+  const handleWebhookNotification = async (req, res) => {
+    try {
+      // Verificar la autenticidad de la notificación usando el header 'x-idempotency-key'
+      const idempotencyKey = req.headers['x-idempotency-key'];
+  
+      // Si es una notificación válida y auténtica, Mercado Pago enviará un payload en el cuerpo de la solicitud
+      const notification = req.body;
+  
+      // Obtener el payment_id y el nuevo estado del payload de la notificación
+      const { id: paymentId, status: newPaymentStatus } = notification.data;
+  
+      // Buscar la orden en la base de datos por el payment_id
+      const order = await Purchase.findOne({ payment_id: paymentId });
+  
+      if (!order) {
+        return res.status(404).json({ message: 'Orden no encontrada' });
+      }
+  
+      // Actualizar el estado de pago de la orden con el nuevo estado
+      order.payment_status = newPaymentStatus;
+      await order.save();
+  
+      // Responder a la notificación de Mercado Pago
+      res.status(200).send('Notificación recibida y procesada correctamente');
+    } catch (error) {
+      console.error('Error al procesar la notificación del webhook:', error);
+      res.status(500).json({ error: 'Error al procesar la notificación del webhook' });
+    }
+  };
 
-export { createPurchase, getOrderDetails };
+  export { createPurchase, getOrderDetails, getUserOrders, handleWebhookNotification };
--- controllers/cart/removeCartItems.js
+++ controllers/cart/removeCartItems.js
@@ -1,20 +1,37 @@
-import Cart from '../../models/Cart.js'
+import Cart from '../../models/Cart.js';
+import Product from '../../models/Product.js'; // Importa el modelo de Product
 
 export default async (req, res) => {
-    const { user_id, productId } = req.body;
-  
-    try {
-      const cart = await Cart.findOne({ user_id });
-  
-      if (!cart) {
-        return res.status(404).json({ error: 'Cart not found' });
-      }
-  
-      cart.items = cart.items.filter(item => !item.product.equals(productId));
-      await cart.save();
-      res.json({ message: 'Cart item removed' });
-    } catch (error) {
-      console.error('Error removing cart item:', error);
-      res.status(500).json({ error: 'Error removing cart item' });
+  const { user_id, productId } = req.body;
+
+  try {
+    const cart = await Cart.findOne({ user_id });
+
+    if (!cart) {
+      return res.status(404).json({ error: 'Cart not found' });
     }
-  };
+
+    // Encuentra el producto en el carrito
+    const cartItem = cart.items.find(item => item.product.equals(productId));
+
+    if (!cartItem) {
+      return res.status(404).json({ error: 'Product not found in cart' });
+    }
+
+    // Restaura la cantidad al stock del producto
+    const product = await Product.findById(cartItem.product);
+    if (product) {
+      product.stock += cartItem.quantity;
+      await product.save();
+    }
+
+    // Filtra el producto del carrito
+    cart.items = cart.items.filter(item => !item.product.equals(productId));
+    await cart.save();
+
+    res.json({ message: 'Cart item removed and stock restored' });
+  } catch (error) {
+    console.error('Error removing cart item:', error);
+    res.status(500).json({ error: 'Error removing cart item' });
+  }
+};
